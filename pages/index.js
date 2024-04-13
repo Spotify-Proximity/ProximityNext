a7eb44a437183975fbcaec6ac6d31c9763e4212a
Kshitij Kochhar <kshitij@ctfguide.com>
2024-04-13 02:21:09 -0400

    Webpage Initialized

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+
+        <>
+            <Head>
+                <title>Home</title>
+                <meta name="description" content="Home page description" />
+            </Head>
+            <div>
+                <h1>Home</h1>
+                <p>Welcome to the home page!</p>
+            </div>
+        </>
+    );
+
+}
